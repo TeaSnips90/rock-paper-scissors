971ef3178539130acb3fbe1c4c589d2ff2e5e191
Teodora <teodora.snips@gmail.com>
2021-11-10 00:42:23 +0100

    Edit script.js

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 function computerPlay(){
+
     let num = Math.floor(Math.random()*3);
-    console.log(num);
-    
     let answer;
+    
     switch(num){
         case 0:
-            answer = "Rock!";
+            answer = "Rock";
             break;
         case 1:
-            answer = "Paper!";
+            answer = "Paper";
             break;
         case 2:
-            answer = "Scissors!";
+            answer = "Scissors";
             break;
     }
-    console.log(answer);
-
+    return answer;
 }
 
+let answer = computerPlay();
+console.log(answer);
+
+
+function playRound(playerSelection, computerSelection){
+
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
+    if(player === computer){
+        console.log("It is a tie!");
+
+    }else if(player == "rock" && computer == "scissors" || player == "papper" && computer == "rock" || player == "scissors" && computer == "papper"){
+        console.log("You win this round!");
+        
+    }else{
+        console.log("I win!");
+    }
+}
+let playerSelection = computerPlay();
+let computerSelection = "papper";
+playRound(playerSelection,computerSelection);
 computerPlay();
-/*console.log(randomNumber());*/
